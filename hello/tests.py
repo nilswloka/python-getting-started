91f165e56c5e98ef58a79f9e43d64391fcdb4dc9
Nils Wloka <nils.wloka@codecentric.de>
2016-06-04 00:05:00 +0200

    New test

--- hello/tests.py
+++ hello/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
+class SimpleTestCase(TestCase):
+    def test_succeed(self):
+        """This test is only for checking my CirceCI setup"""
+        self.assertEqual(1, 1)
